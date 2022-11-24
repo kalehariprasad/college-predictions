679fcd7c9c5ca1eec0367f89d14081be99ce2729
kalehariprasad <106798714+kalehariprasad@users.noreply.github.com>
2022-11-24 22:48:58 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -12,7 +12,7 @@ import sklearn
 import os
 
 dirname = os.path.dirname(__file__)
-model= os.path.join(dirname, 'pickle.load(open('C:/Users/User/Documents/college_prediction/pipe2.pkl','rb' )))
+model= os.path.join(dirname, pickle.load(open('C:/Users/User/Documents/college_prediction/pipe2.pkl','rb' )))
 st.title('Admission Prediction')
 def user_input():
     school=st.selectbox('course type', ('Academic', 'Vocational'))
